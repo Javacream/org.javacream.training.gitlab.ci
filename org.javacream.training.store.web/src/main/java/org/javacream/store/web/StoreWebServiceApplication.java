58e8a0788d78fdc7e92cd10710028ae564305d11
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-06-15 16:05:02 +0200

    add webservice

---
+++ org.javacream.training.store.web/src/main/java/org/javacream/store/web/StoreWebServiceApplication.java
@@ -0,0 +1,67 @@
+package org.javacream.store.web;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+@RequestMapping(path = "api")
+public class StoreWebServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StoreWebServiceApplication.class, args);
+	}
+	
+	@PersistenceContext private EntityManager entityManager;
+
+	@GetMapping(path = "store/{category}/{item}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String getStock(@PathVariable("category") String category, @PathVariable("item") String item) {
+		return Integer.toString(readStock(category, item));
+	}
+
+	@PostMapping(path = "store/{category}/{item}", produces = MediaType.TEXT_PLAIN_VALUE)
+	@Transactional
+	public void setStock(@PathVariable("category") String category, @PathVariable("item") String item,
+			@RequestHeader("stock") int stock) {
+		saveStock(category, item, stock);
+	}
+	
+	private void saveStock(String category, String id, int stock) {
+		if (category == null || id == null) {
+			throw new IllegalArgumentException("category and id must not be null");
+		}
+		StoreId storeId = new StoreId(category, id);
+		
+		StoreEntry storeEntry = entityManager.find(StoreEntry.class, storeId);
+		if (storeEntry == null) {
+			entityManager.persist(new StoreEntry(category, id, stock));
+		}else {
+			entityManager.merge(new StoreEntry(category, id, stock));
+		}
+	}	
+	private int readStock(String category, String id) {
+		try {
+			Query query = entityManager.createQuery("select stock from StoreEntry where category = :category and itemId = :id");
+			query.setParameter("category", category);
+			query.setParameter("id", id);
+			Integer result = (Integer) query.getSingleResult();
+			return result;
+		}
+		catch(Exception e) {
+			return 0;
+		}
+	}
+
+}
